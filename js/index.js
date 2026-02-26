ef2d353f8caa89578252754605564d78094deb9c
Arthur Heiji Voos Shiroshima <arthurshiro123@gmail.com>
2026-02-26 20:45:36 -0300

    fix: Consertado bug de clique duplo e ausência de reset das variáveis

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const words = ["Mario", "Luigi", "Wario", "Waluigi", "Peach", "Bowser"];
-let revealedCards = []
 const btnRestart = document.getElementById("btnRestart");
 const cards = document.querySelectorAll(".card");
 var first = null;
@@ -7,55 +6,70 @@ var second = null;
 let tries = 0;
 var blocked = false;
 
-start()
-btnRestart.onclick = () => start();
-
 function start() {
-    let scrambledWords = scramble([...words, ...words]);
+  let scrambledWords = scramble([...words, ...words]);
 
-    cards.forEach((card, i) => {
-        card.textContent = "?";
-        card.classList.remove("selected")
-        card.dataset.word = scrambledWords[i];
-        card.onclick = () => reveal(card);
-    })
+  first = null;
+  second = null;
+  tries = 0;
+  blocked = false;
+
+  cards.forEach((card, i) => {
+    delete card.dataset.matched;
+    card.textContent = "?";
+    card.classList.remove("selected");
+    card.dataset.word = scrambledWords[i];
+    card.onclick = () => reveal(card);
+  });
 }
 
 function check() {
-    if (first.textContent == second.textContent) {
-        first = null;
-        second = null;
-        console.log("Acertou");
-    } else {
-        blocked = true;
-        setTimeout(() => {
-            first.textContent = "?";
-            second.textContent = "?";
-            first.classList.remove("selected")
-            second.classList.remove("selected")
-            first = null;
-            second = null;
-        }, 1000)
-        blocked = false;
-    }
+  if (first.textContent == second.textContent) {
+    first.dataset.matched = true;
+    second.dataset.matched = true;
+
+    first = null;
+    second = null;
+    console.log("Acertou");
+  } else {
+    blocked = true;
+    setTimeout(() => {
+      first.textContent = "?";
+      second.textContent = "?";
+      first.classList.remove("selected");
+      second.classList.remove("selected");
+      first = null;
+      second = null;
+      blocked = false;
+    }, 500);
+  }
 }
 
 function reveal(card) {
-    card.textContent = card.dataset.word;
-    card.classList.add("selected")
-    if (!first) {
-        first = card;
-        return;
-    }
-    second = card;
-    check();
-    tries++;
+  if (card.dataset.matched || blocked) {
+    return;
+  }
+  if (card == first) {
+    return;
+  }
+  card.textContent = card.dataset.word;
+  card.classList.add("selected");
+  if (!first) {
+    first = card;
+    return;
+  }
+  second = card;
+  check();
+  tries++;
 }
 
 function scramble(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (1 + i));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (1 + i));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
+
+start();
+btnRestart.onclick = () => start();
